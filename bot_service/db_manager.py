3e24d02a554bf8d0d400936d52e5bdc9c23ae3d0
Dias Bekbolatuly <dirrayoo@gmail.com>
2025-12-06 21:24:59 +0500

    Remove unused user ban functions from db_manager.py
    Removed ban_user and check_permission functions from db_manager.py, along with unban_user function which was not used.

--- bot_service/db_manager.py
+++ bot_service/db_manager.py
@@ -1,5 +1,5 @@
 import datetime
-from .config import CHAT_COLLECTION, HISTORY_LIMIT, BAN_COLLECTION
+from .config import CHAT_COLLECTION, HISTORY_LIMIT
 
 db = None
 
@@ -11,27 +11,6 @@ async def save_message(user_id: int, role: str, text: str):
             "text": text,
             "timestamp": datetime.datetime.now()
         })
-
-async def ban_user(user_id: int, full_name: str):
-    if db is not None:
-        await db[BAN_COLLECTION].insert_one({
-            "user_id": user_id,
-            "full_name": full_name,
-            "timestamp": datetime.datetime.now()
-        })
-
-async def check_permission(user_id: int):
-    if db is None:
-        return 0
-
-    banned_users = db[BAN_COLLECTION].find_one(
-        {"user_id": user_id}
-    )
-
-    if banned_users is None:
-        return 0
-
-    return 1
 
 
 async def get_chat_history(user_id: int):
@@ -66,10 +45,3 @@ async def clear_chat_history(user_id: int):
         })
         return result.deleted_count
     return 0
-
-# реализовал, но нигде не применяется
-async def unban_user(user_id: int):
-    if db is not None:
-        await db[BAN_COLLECTION].delete_one({
-            "user_id": user_id
-        })
